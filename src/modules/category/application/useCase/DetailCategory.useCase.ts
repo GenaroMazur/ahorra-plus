42ab636f3444b93caf31c4804d058608ce9090b5
GenaroMazur <genaromazur.business@gmail.com>
2025-09-22 14:15:59 -0300

    feat(category): implementación de casos de uso

---
+++ src/modules/category/application/useCase/DetailCategory.useCase.ts
@@ -0,0 +1,13 @@
+import CategoryUseCase from "./Category.useCase";
+import {UseCaseInterface} from "../../../../share/interfaces/UseCase.interface";
+import CategoryNotFoundException from "../../domain/exceptions/CategoryNotFound.exception";
+
+export default class DetailCategoryUseCase extends CategoryUseCase implements UseCaseInterface {
+    async execute(userId: number, categoryId: number) {
+        const category = await this.categoryRepository.findByIdAndUserId(categoryId, userId)
+
+        if (!category) throw new CategoryNotFoundException(categoryId)
+
+        return category
+    }
+}
--- src/modules/category/domain/repository/Category.repository.ts
+++ src/modules/category/domain/repository/Category.repository.ts
@@ -2,4 +2,7 @@ import RepositoryInterface from "../../../../share/interfaces/Repository.interfa
 import Category from "../entity/Category";
 
 export interface CategoryRepository extends RepositoryInterface<Category> {
+    findByIdAndUserId(id: number, userId: number): Promise<Category | null>;
+
+    findByUserId(userId: number): Promise<Category[]>;
 }
