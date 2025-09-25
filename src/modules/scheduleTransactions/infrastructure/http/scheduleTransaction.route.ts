58b65bd95422260b0fb4425d9930526211a42094
GenaroMazur <genaromazur.business@gmail.com>
2025-09-25 17:52:37 -0300

    feat(scheduleTransaction): implement CRUD operations for schedule transactions

--- src/modules/scheduleTransactions/infrastructure/http/scheduleTransaction.route.ts
+++ src/modules/scheduleTransactions/infrastructure/http/scheduleTransaction.route.ts
@@ -1,9 +1,18 @@
 import {Router} from "express";
+import {
+    createScheduleTransactionsController, deleteScheduleTransactionsController, detailScheduleTransactionsController,
+    listScheduleTransactionsController, updateScheduleTransactionsController
+} from "./scheduleTransaction.controller";
 
 const scheduleTransactionRoute = Router({mergeParams: true});
 
-// scheduleTransactionRoute.route("/").get().post()
-//
-// scheduleTransactionRoute.route("/:id").get().patch().delete()
+scheduleTransactionRoute.route("/")
+    .get(listScheduleTransactionsController)
+    .post(createScheduleTransactionsController)
+
+scheduleTransactionRoute.route("/:id")
+    .get(detailScheduleTransactionsController)
+    .patch(updateScheduleTransactionsController)
+    .delete(deleteScheduleTransactionsController)
 
 export default scheduleTransactionRoute;
